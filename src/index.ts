627bcc47f2d20aaecea348706c5d217ef2bb0e95
Mayank <mayankbestbro@gmail.com>
2022-12-06 12:37:02 +0530

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -30,36 +30,36 @@ export class flasho {
 
     static async createSmsTrigger(requestBody: SmsRequestBodyType, config:any) {
         try {
-            const response = await axios.post(`http://localhost:8000/api/v1/manual_sms/send_new_manual_templated_sms`, JSON.stringify(requestBody), config);
+            const response:any = await axios.post<any>(`http://localhost:8000/api/v1/manual_sms/send_new_manual_templated_sms`, JSON.stringify(requestBody), config);
             return response.data;
-          } catch (error) {
+          } catch (error:any) {
             console.error(error);
           }
     };
 
     static async createEmailTrigger(requestBody: EmailRequestBodyType, config:any) {
         try {
-            const response = await axios.post(`http://localhost:8000/api/v1/manual_email/send_new_manual_templated_email`, JSON.stringify(requestBody), config);
+            const response:any = await axios.post<any>(`http://localhost:8000/api/v1/manual_email/send_new_manual_templated_email`, JSON.stringify(requestBody), config);
             return response.data;
-          } catch (error) {
+          } catch (error:any) {
             console.error(error);
           }
     };
 
     static async getSmsTrigger(triggerName: string, config:any) {
         try {
-            const response = await axios.get(`http://localhost:8000/api/v1/manual_sms/${triggerName}`, config);
+            const response:any = await axios.get<any>(`http://localhost:8000/api/v1/manual_sms/${triggerName}`, config);
             return response.data;
-          } catch (error) {
+          } catch (error:any) {
             console.error(error);
           }
     };
 
     static async getEmailTrigger(triggerName: string, config:any){
         try {
-            const response = await axios.get(`http://localhost:8000/api/v1/manual_emial/${triggerName}`, config);
+            const response:any = await axios.get<any>(`http://localhost:8000/api/v1/manual_emial/${triggerName}`, config);
             return response.data;
-          } catch (error) {
+          } catch (error:any) {
             console.error(error);
           }
     };
