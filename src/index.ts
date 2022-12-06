65920d582bd7956a03f878b6c76e8b99d03cea62
Mayank <mayankbestbro@gmail.com>
2022-12-06 12:33:18 +0530

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ export class flasho {
     static async createSmsTrigger(requestBody: SmsRequestBodyType, config:any) {
         try {
             const response = await axios.post(`http://localhost:8000/api/v1/manual_sms/send_new_manual_templated_sms`, JSON.stringify(requestBody), config);
-            return response;
+            return response.data;
           } catch (error) {
             console.error(error);
           }
@@ -40,7 +40,7 @@ export class flasho {
     static async createEmailTrigger(requestBody: EmailRequestBodyType, config:any) {
         try {
             const response = await axios.post(`http://localhost:8000/api/v1/manual_email/send_new_manual_templated_email`, JSON.stringify(requestBody), config);
-            return response;
+            return response.data;
           } catch (error) {
             console.error(error);
           }
@@ -49,7 +49,7 @@ export class flasho {
     static async getSmsTrigger(triggerName: string, config:any) {
         try {
             const response = await axios.get(`http://localhost:8000/api/v1/manual_sms/${triggerName}`, config);
-            return response;
+            return response.data;
           } catch (error) {
             console.error(error);
           }
@@ -58,7 +58,7 @@ export class flasho {
     static async getEmailTrigger(triggerName: string, config:any){
         try {
             const response = await axios.get(`http://localhost:8000/api/v1/manual_emial/${triggerName}`, config);
-            return response;
+            return response.data;
           } catch (error) {
             console.error(error);
           }
